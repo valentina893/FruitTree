8ddb215b4c8e9e3e1a04b6fad7b07ae700c48708
valentina893 <valentinaramirezsusarret@gmail.com>
2024-06-27 12:05:51 -0500

    new iteration with mango's morphing ability

---
+++ main2.py
@@ -0,0 +1,280 @@
+import time, random, copy
+
+def t1():
+    time.sleep(1)
+
+def m_special(user_dct, en_dct):
+
+    if user_dct['stats'][-3] == False:
+        original_dct = copy.deepcopy(user_dct)
+        new_dct = copy.deepcopy(en_dct)
+        user_dct.clear()
+        user_dct.update(copy.deepcopy(new_dct))
+        user_dct['stats'][-2] = m_special
+        user_dct['stats'][-1] = original_dct
+        t1()
+        print('mango has morphed into', en_dct['stats'][2])
+    elif user_dct['stats'][-3] == True:
+        original = user_dct['stats'][-1]
+        user_dct.clear()
+        user_dct.update(copy.deepcopy(original))
+        t1()
+        print("mango's morph special has ended.")
+
+    return user_dct
+
+def b_special():
+
+    pass
+
+def s_special():
+
+    pass
+
+def p1_reg_move():
+
+    t1()
+    print('__________________')
+    print(p1c_dict['stats'][2], ':', p1c_dict[list(p1c_dict.keys())[0]][-1], p1c_dict[list(p1c_dict.keys())[1]][-1], '\nhp & power:', p1c_dict['stats'][0], p1c_dict['stats'][1])
+    print('------------------')
+
+    u_inp = input().lower()
+
+    if u_inp == list(p1c_dict.keys())[0]: # regular move
+        if p1c_dict[list(p1c_dict.keys())[0]][0] > 0:
+            p1c_dict[list(p1c_dict.keys())[0]][0] -= 1
+            return (p1c_dict[list(p1c_dict.keys())[0]], p1c_dict)
+        else:
+            return (False, p1c_dict)
+    elif u_inp == list(p1c_dict.keys())[1]:
+        if p1c_dict['stats'][1] > 0:
+            p1c_dict['stats'][1] -= 1
+            return (p1c_dict[list(p1c_dict.keys())[1]], p1c_dict)
+        else:
+            return (False, p1c_dict)
+    else:
+        return (False, p1c_dict)
+    
+def en_reg_move():
+# should ai be smart enough to not choose useless attacks? --> ai will be stupid for now
+    en_inp = random.choice([1, 2, 3])
+
+    return ([1, 2, 3, 4, 5], p2c_dict)
+
+def p1_tag_move():
+
+    u_inp = input()
+
+    pass
+
+def en_tag_move():
+
+    pass
+
+def p2_reg_move():
+
+    u_inp = input()
+
+    pass
+
+def p2_tag_move():
+    
+    u_inp = input()
+
+    pass
+
+def damage(dam, dct):
+
+    dct['stats'][0] -= dam
+
+    return dct
+
+def combat(): # takes player choices as values, flips coin to see who goes first using values, uses functions to manipulate health and stats
+
+    global p1c_dict, p2c_dict
+
+    p1 = p1_move()
+    p2 = p2_move()
+
+    if p1[0] != False:
+        if p1[0][-2] == 'regular':
+            t1()
+            print('player 1 used', p1[0][-1])
+            t1()
+            print('player 2 used', p2[0][-1])
+            p2c_dict = damage(p1[0][1], p2[1])
+            if p1c_dict['stats'][-3] == True:
+                p1c_dict['stats'][-4] += 1
+        else:
+            t1()
+            print('player 1 used a special move:', p1[0][-1])
+            if p1c_dict['stats'][-3] == False:
+                p1c_dict['stats'][3](p1c_dict, p2c_dict)
+                p1c_dict['stats'][-3] = True
+            else:
+                p1c_dict['stats'][-4] += 1
+            t1()
+            print('player 2 used', p2[0][-1])
+    elif p1[0] == False:
+        t1()
+        print('player 1 is invalid')
+        t1()
+        print('player 2 used', p2[0][-1])
+
+    if p1c_dict['stats'][-4] == 5:
+        p1c_dict['stats'][-2](p1c_dict, p2c_dict)
+        p1c_dict['stats'][-3] = False
+
+    t1()
+    print('p2 health is', p2c_dict['stats'][0])
+
+mango = { # name : pp, damage, speed-weight, crit, miss, call
+           'h':[5, 10, 0.5, (1,6), (1,8), 'regular', 'hit'],
+           'm':[None, 0, 0.3, None, None, 'special', 'morph'],
+           'stats':[100, 1, 'mango', m_special, 0, False, None, None],
+          } # health, power, name, specials * 3, special_cnt, special_bool, reversal_foo_indicator
+
+banana = {
+           'k':[10, 10, 0.5, (1,6), (1,8), 'regular', 'kick'],
+           's':[5, 20, 0.2, (3,6), (2,8), 'special', 'shoot'],
+           'stats':[100, 1, 'banana', b_special, 0, False, None, None]
+          }
+
+strawberry = {
+            'p':[10, 10, 0.5, (1,6), (1,8), 'regular', 'pound'],
+            'x':[5, 0, 0.3, None, None, 'special', 'x'], #healing ability
+            'stats':[100, 1, 'strawberry', s_special, 0, False, None, None]
+        }
+
+char_sel = {
+            'm':['mango', mango],
+            'b':['banana', banana],
+            's':['strawberry', strawberry],
+           }
+
+g_mode_sel = {
+              (1, 'r'):[1, 'regular', p1_reg_move, en_reg_move],
+              (1, 't'):[1, 'tag', p1_tag_move, en_tag_move],
+              (2, 'r'):[2, 'regular', p1_reg_move, p2_reg_move],
+              (2, 't'):[2, 'tag', p1_tag_move, p2_tag_move]
+             }
+
+g_mode_choice = (None, None) # holds data of amt of players, game mode
+char_choice1 = [] # holds character data for player 1
+char_choice2 = [] # holds character data for player 2 (real or bot)
+
+print('fruit tree') # start of game
+t1()
+print('1 player or 2 players?')
+
+while g_mode_choice == (None, None): # chooses the amt of players and game mode
+    p_amt = int(input())
+    if p_amt == 1 or 2:
+        g_mode_choice = (p_amt, None)
+        print('regular or tag mode?')
+        while g_mode_choice == (p_amt, None):
+            m_choice = input().lower()
+            if m_choice in ['r', 't']:
+                g_mode_choice = (p_amt, m_choice) 
+            else:
+                print('not valid')
+                t1()
+                print('regular or tag mode?')
+    else:
+        print('not valid')
+        t1()
+        print('1 player or 2 players?')
+
+t1()
+print('game mode:', g_mode_sel[g_mode_choice][0], 'player,', g_mode_sel[g_mode_choice][1], 'mode') # states the selected amount of players and game mode
+
+t1()
+char_sel_display = [char_sel[list(char_sel.keys())[0]][0], char_sel[list(char_sel.keys())[1]][0], char_sel[list(char_sel.keys())[2]][0]]
+print(char_sel_display)
+
+if g_mode_choice[1] == 'r': # chooses characters if in regular mode
+    while len(char_choice1) < 1:
+        p1_char1 = input().lower()
+        if p1_char1 in char_sel.keys():
+            char_choice1.append(p1_char1)
+            p1c_dict = copy.deepcopy(char_sel[char_choice1[0]][1])
+            p1c_name = char_sel[char_choice1[0]][0]
+        else:
+            print('not valid')
+        if g_mode_choice[0] == 2:
+            while len(char_choice2) < 1:
+                p2_char1 = input().lower()
+                if p2_char1 in char_sel.keys():
+                        char_choice2.append(p2_char1)
+                else:
+                    print('not valid')
+        else:
+            p2_char1 = random.choice(list(char_sel.keys())[1:])
+            char_choice2.append(p2_char1)
+        p2c_dict = copy.deepcopy(char_sel[char_choice2[0]][1])
+        p2c_name = char_sel[char_choice2[0]][0]
+    t1()
+    print(char_sel[p1_char1][0], 'vs.', char_sel[p2_char1][0]) # states the selected characters (reg mode)
+
+else: # char_sel for tag mode
+    while len(char_choice1) < 1:
+        choice_lst1 = list(char_sel.keys())
+        p1_char1 = input().lower()
+        if p1_char1 in choice_lst1:
+            char_choice1.append(p1_char1)
+            choice_lst1.remove(p1_char1)
+            p1c_dict = copy.deepcopy(char_sel[char_choice1[0]][1])
+            p1c_name = char_sel[char_choice1[0]][0]
+            while len(char_choice1) < 2:
+                p1_char2 = input().lower()
+                if p1_char2 in choice_lst1:
+                    char_choice1.append(p1_char2)
+                    p1c2_dict = copy.deepcopy(char_sel[char_choice1[1]][1])
+                    p1c2_name = char_sel[char_choice1[1]][0]
+                else:
+                    print('not valid')
+        else:
+            print('not valid')
+    if g_mode_choice[0] == 2: # char_sel for 2t
+        while len(char_choice2) < 1:
+            choice_lst2 = list(char_sel.keys())
+            p2_char1 = input().lower()
+            if p2_char1 in choice_lst2:
+                char_choice2.append(p2_char1)
+                choice_lst2.remove(p2_char1)
+                while len(char_choice2) < 2:
+                    p2_char2 = input().lower()
+                    if p2_char2 in choice_lst2:
+                        char_choice2.append(p2_char2)
+                    else:
+                        print('not valid')
+            else:
+                print('not valid')
+    else: # char_sel for 1t
+        choice_lst2 = list(char_sel.keys())
+        p2_char1 = random.choice(choice_lst2)
+        char_choice2.append(p2_char1)
+        choice_lst2.remove(p2_char1)
+        p2_char2 = random.choice(choice_lst2)
+        char_choice2.append(p2_char2)
+    p2c_dict = copy.deepcopy(char_sel[char_choice2[0]][1])
+    p2c_name = char_sel[char_choice2[0]][0]
+    p2c2_dict = copy.deepcopy(char_sel[char_choice2[1]][1])
+    p2c2_name = char_sel[char_choice2[1]][0]
+    t1()
+    print(char_sel[p1_char1][0], '&', char_sel[p1_char2][0], 'vs.', char_sel[p2_char1][0], '&', char_sel[p2_char2][0]) # states the selected tag teams
+
+p1_move = g_mode_sel[g_mode_choice][2]
+p2_move = g_mode_sel[g_mode_choice][3]
+
+special1 = False
+
+t1()
+print('duel!')
+
+Game = True
+
+while Game == True:
+    combat()
+    if p2c_dict['stats'][0] <= 0:
+        Game = False
